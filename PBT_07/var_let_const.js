2067d341e50eec5c93c13d5964816ccb14bd216e
Vũ Thị Lê Vy <vyvu2006@gmail.com>
2026-05-21 06:41:03 +0700

    [FEATURE] Add var let const comparison examples

---
+++ PBT_07/var_let_const.js
@@ -0,0 +1,25 @@
+// Đoạn 1
+console.log(x);
+var x = 5;
+
+// Đoạn 2
+console.log(y);
+let y = 10;
+
+// Đoạn 3
+const z = 15;
+z = 20;
+console.log(z);
+
+// Đoạn 4
+const arr = [1, 2, 3];
+arr.push(4);
+console.log(arr);
+
+// Đoạn 5
+let a = 1;
+{
+  let a = 2;
+  console.log("Trong block:", a);
+}
+console.log("Ngoài block:", a);
